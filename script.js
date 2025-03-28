383fe9bcba062af35274d6ada8199df016452fc9
vishnuviratGit <vishnuvirat564@gmail.com>
2025-03-28 13:43:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 function insert_Row() {
     //Write your code here
-  
+    let table=document.getElementById("sampleTable");
+	let newRow=table.insertRow(0);
+	let cell1=newRow.insertCell(0);
+	let cell2=newRow.insertCell(1);
+	cell1.textContent="New Cell1";
+	cell2.textContent="New Cell2";
   
 }
